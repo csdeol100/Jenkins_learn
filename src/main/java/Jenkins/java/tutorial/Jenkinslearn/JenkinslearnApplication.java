dd1ca72f8861b63a7d04d6edadfd4b26f27f3f60
Charandeep Deol <csdeol100@gmail.com>
2021-01-22 11:48:09 -0500

    First Commit

---
+++ src/main/java/Jenkins/java/tutorial/Jenkinslearn/JenkinslearnApplication.java
@@ -0,0 +1,13 @@
+package Jenkins.java.tutorial.Jenkinslearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinslearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinslearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Jenkins/java/tutorial/Jenkinslearn/JenkinslearnApplicationTests.java
@@ -0,0 +1,13 @@
+package Jenkins.java.tutorial.Jenkinslearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinslearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
